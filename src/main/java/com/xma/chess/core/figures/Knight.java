6e0bbc0777968374b9a22cac163797aaf85a06d4
XMA <xma4602@gmail.com>
2023-09-27 18:55:02 +0400

    Созданы классы затычки для обработки действий фигур.

---
+++ src/main/java/com/xma/chess/core/figures/Knight.java
@@ -0,0 +1,18 @@
+package com.xma.chess.core.figures;
+
+import com.xma.chess.core.Board;
+import com.xma.chess.core.Position;
+import com.xma.chess.core.actions.Action;
+
+import java.util.List;
+
+public class Knight extends Figure{
+    public Knight(Board board) {
+        super(board);
+    }
+
+    @Override
+    public List<Action> getActions(Position position) {
+        return null;
+    }
+}
